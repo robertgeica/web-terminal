4e1d175b12c1512b185dfbf65e3a459d3e4b8906
mr robot <geicarobert@gmail.com>
2020-09-28 21:37:21 +0300

    Updates

---
+++ precache-manifest.4d7a8282396d9782451f8d8b2e41e265.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f34a0e1e1620ca84a31c5af6f6ec1ab",
+    "url": "/web-terminal/index.html"
+  },
+  {
+    "revision": "188d8dd10b975e38f26b",
+    "url": "/web-terminal/static/css/main.a2c9fef7.chunk.css"
+  },
+  {
+    "revision": "1585fb2578a06153306f",
+    "url": "/web-terminal/static/js/2.017c5e04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-terminal/static/js/2.017c5e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "188d8dd10b975e38f26b",
+    "url": "/web-terminal/static/js/main.276d0f32.chunk.js"
+  },
+  {
+    "revision": "3bfbbc85157a55e28a93",
+    "url": "/web-terminal/static/js/runtime-main.ac248ebd.js"
+  }
+]);
